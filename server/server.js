29ac78fc93dcec4495ff622cd3afbaaa850b1562
udarajayawardena5@gmail.com <1925uj7225>
2019-09-30 09:01:28 +0530

    User Connnection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const path = require('path');
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-const express = require('express');
-
 var app = express();
+var server = http.createServer(app)
+
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('connected socket ID : ' + socket.id);
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user is disconnected')
+    })
+})
+
 app.use(express.static(publicPath));
 
-
-app.listen(port, () => {
-    console.log('server is running on port : ' +port)
+server.listen(port, () => {
+    console.log('server is running on port : ' + port)
 })
