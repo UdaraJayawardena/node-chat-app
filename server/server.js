1ffd37e1e69924bc6c0f9cb57c9671ff585b5334
udarajayawardena5@gmail.com <1925uj7225>
2019-10-01 10:10:47 +0530

    Mongodb Removed

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
-const mongo = require('mongodb').MongoClient;
 
 const {genarateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
@@ -13,52 +12,44 @@ var app = express();
 var server = http.createServer(app)
 var io = socketIO(server);
 
-mongo.connect('mongodb://127.0.0.1/data', function (err, db) {
-    if (err) {
-        throw err;
-    }
 
-    console.log('MongoDB connected...');
+io.on('connection', (socket) => {
 
-    io.on('connection', (socket) => {
+    console.log('new user connected');
 
-        let chat = db.collection('sample');
+    socket.emit('newMessage', genarateMessage('Admin', 'Welcome to the Chat App'))
 
-        console.log('new user connected');
+    socket.broadcast.emit('newMessage', genarateMessage('Admin', 'New User Join'))
 
-        socket.emit('newMessage', genarateMessage('Admin', 'Welcome to the Chat App'))
+    socket.on('createMessage', (message) => {
+        console.log('Message : ', message);
 
-        socket.broadcast.emit('newMessage', genarateMessage('Admin', 'New User Join'))
+        io.emit('newMessage', genarateMessage(message.from, message.text))
 
-        socket.on('createMessage', (message) => {
-            console.log('Message : ', message);
-
-            io.emit('newMessage', genarateMessage(message.from, message.text))
-
-            // socket.broadcast.emit('newMessage', {
-            //     'from': message.from,
-            //     'text': message.text,
-            //     createAt: new Date().getTime()
-            // })
-
-        })
-
-        socket.on('disconnect', () => {
-            console.log('user is disconnected')
-        })
-
-        // socket.on('createAccount', (account) => {
-        //     console.log(account)
-        //     chat.insert({
-        //         account
-        //     }, () => {
-        //         console.log('insert')
-        //     })
+        // socket.broadcast.emit('newMessage', {
+        //     'from': message.from,
+        //     'text': message.text,
+        //     createAt: new Date().getTime()
         // })
 
-
     })
+
+    socket.on('disconnect', () => {
+        console.log('user is disconnected')
+    })
+
+    // socket.on('createAccount', (account) => {
+    //     console.log(account)
+    //     chat.insert({
+    //         account
+    //     }, () => {
+    //         console.log('insert')
+    //     })
+    // })
+
+
 })
+
 
 app.use(express.static(publicPath));
 
